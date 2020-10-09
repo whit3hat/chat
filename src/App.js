d4823bf0c56f7ab9ed6e62ea2261f3a4fa9e37d6
Spencer Wood <spencer.wood@me.com>
2020-10-08 20:55:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  //Config
+  apiKey: "AIzaSyCUCKCwdG20xSzFD5_IVtYWk4YwRiR1A7A",
+    authDomain: "chat-f5015.firebaseapp.com",
+    databaseURL: "https://chat-f5015.firebaseio.com",
+    projectId: "chat-f5015",
+    storageBucket: "chat-f5015.appspot.com",
+    messagingSenderId: "756514560303",
+    appId: "1:756514560303:web:c142737eab6775ea8fa9da"
+})
+
+const [user] = useAuthState(auth);
+
+//Global Variables
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+
+  function SignIn() {
+    const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+    
+    return (
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    )
+
+    function SignOut() {
+      
+    }
+  }
+
+  function ChatRoom() {
+
+  }
 }
 
 export default App;
